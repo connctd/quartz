51310ea24446f8e5082f764886179d1c6ad785e9
Pandelis Zembashis <info@pandelis.me>
2019-08-06 12:31:26 +0200

    margin collapse fix

--- components/theme/GlobalStyles.ts
+++ components/theme/GlobalStyles.ts
@@ -4,11 +4,17 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyle: React.FC = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Hind:400,600,700&display=swap');
 
-  body,html,#root {
+  body,html {
     margin: 0 auto;
     padding: 0;
     height: 100%;
     font-family: 'Hind', Helvetica Neue, Helvetica, Segoe UI, Arial, freesans, sans-serif;
+  }
+
+  #root { /* Margin collapse fix  */
+    padding: 1px 0 0 0;
+    margin: -1px auto 0 auto;
+    height: 100%;
   }
 
   input, select, textarea, button { font-family: 'Hind', Helvetica Neue, Helvetica, Segoe UI, Arial, freesans, sans-serif; outline: none; }
