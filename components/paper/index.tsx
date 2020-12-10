36cc57d4a05258625523eef44270c2a944ad296d
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2020-12-10 20:13:13 +0100

    feat: adjust Paper component

--- components/paper/index.tsx
+++ components/paper/index.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
+
 import { Themeable } from '../theme';
 
 export interface PaperProps extends Themeable {
   children: React.ReactNode;
-  narrow?: boolean;
-  light?: boolean;
 }
 
 const StyledPaper = styled.div<PaperProps>`
   position: relative;
   z-index: 1;
+  padding: 32px;
   width: auto;
-  background: #FFFFFF;
-  box-shadow: ${(props) => (props.light ? '0px 5px 15px rgba(0, 0, 0, 0.1)' : '0px 0px 8px rgba(0, 0, 0, 0.25)')};
-  padding: ${(props) => (props.narrow ? '15px 24px' : '20px 40px')};
+  background: #fff;
+  border: solid 1px #bdbdbd;
+  border-radius: 8px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
 export const Paper: React.FC<PaperProps> = ({ children, ...rest }) => (
