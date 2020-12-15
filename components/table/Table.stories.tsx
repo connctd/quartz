ffa57c9ed81299c493c186692aa31e2a4627725c
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2020-12-15 15:41:52 +0100

    refactor: change api of Table component

--- components/table/Table.stories.tsx
+++ components/table/Table.stories.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
+
 import { Table } from './index';
 
 const stories = storiesOf('Table', module);
+
 stories.addDecorator(withKnobs);
 stories.addDecorator(withInfo);
 stories.addParameters({ info: { inline: true } });
 
-stories.add('Table', () => {
-  const headings = ['Name', 'Created', 'Public', 'Tags', ''];
-  const data = {
-    1: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    2: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    3: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    4: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    5: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    6: ['Production', '01.07.2019', 'No', 'production', 'edit'],
-    7: ['Production', '01.07.2019', 'No', 'production', 'edit']
-  };
-  return <Table data={data} headings={headings} />;
-});
+stories.add('Table', () => (
+  <Table>
+    <Table.Header>
+      <Table.HeaderCell>Header One</Table.HeaderCell>
+      <Table.HeaderCell>Header Two</Table.HeaderCell>
+      <Table.HeaderCell>Header Three</Table.HeaderCell>
+    </Table.Header>
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Cell One</Table.Cell>
+        <Table.Cell>Cell Two</Table.Cell>
+        <Table.Cell>Cell Three</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Cell Four</Table.Cell>
+        <Table.Cell>Cell Five</Table.Cell>
+        <Table.Cell>Cell Six</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+));
--- components/table/index.tsx
+++ components/table/index.tsx
@@ -1,60 +1,133 @@
-/* eslint react/no-array-index-key: 0 */
 import React from 'react';
 import styled from '@emotion/styled';
-import { defaultTheme, Themeable } from '../theme';
 
-interface Table extends Themeable {
-  columns: number;
+import { defaultTheme, QuartzTheme, Themeable } from '../theme';
+
+interface TableProps
+  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement> {
+  children: React.ReactElement<TableHeaderProps | TableBodyProps>[];
+  theme?: QuartzTheme;
 }
 
-const TableBody = styled.div<Table>`
-  display: grid;
-  grid-template-columns: repeat(${(props) => props.columns}, 1fr);
-  grid-row-gap: 12px;
+interface TableHeaderProps
+  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableSectionElement>, HTMLTableSectionElement> {
+  children: React.ReactElement<TableHeaderCellProps>[];
+  theme?: QuartzTheme;
+}
 
-  div {
-    padding: 12px;
-    border-bottom: 1px solid ${(props) => props.theme.light50};
-    &:nth-of-type(${(props) => props.columns}n) {
-      text-align: right;
-    }
-  }
-`;
-const TableHeader = styled.div<Table>`
-  display: grid;
-  background-color: ${(props) => props.theme.light30};
-  border-radius: 2px;
-  border: 1px solid ${(props) => props.theme.light50};
-  grid-template-columns: repeat(${(props) => props.columns}, 1fr);
+interface TableHeaderCellProps
+  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableHeaderCellElement>, HTMLTableHeaderCellElement> {
+  children: React.ReactNode;
+  theme?: QuartzTheme;
+}
 
-  div {
-    padding: 6px 12px 3px 12px;
-  }
-`;
+interface TableBodyProps
+  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableSectionElement>, HTMLTableSectionElement> {
+  children: React.ReactElement<TableRowProps> | React.ReactElement<TableRowProps>[];
+  theme?: QuartzTheme;
+}
 
-const TableContainer = styled.div`
-  display: block;
+interface TableRowProps extends
+  React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement> {
+  children: React.ReactElement<TableCellProps>[];
+  theme?: QuartzTheme;
+}
+
+interface TableCellProps
+  extends React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableCellElement>, HTMLTableCellElement> {
+  children: React.ReactNode;
+  theme?: QuartzTheme;
+}
+
+interface TableSubComponents {
+  Header: React.FunctionComponent<TableHeaderProps>;
+  HeaderCell: React.FunctionComponent<TableHeaderCellProps>;
+  Body: React.FunctionComponent<TableBodyProps>;
+  Row: React.FunctionComponent<TableRowProps>;
+  Cell: React.FunctionComponent<TableCellProps>;
+}
+
+const StyledTable = styled.table(`
+  table-layout: fixed;
   width: 100%;
-`;
+`);
 
-export interface TableProps extends Themeable {
-  headings: string[];
-  data: Record<string, any[]>;
-}
+const StyledTableHeader = styled.thead(`
+  background-color: #f2f2f2;
+  border-radius: 2px;
+`);
 
-export const Table: React.FC<TableProps> = React.memo(({ headings, data, theme }) => (
-  <TableContainer>
-    <TableHeader theme={theme} columns={headings.length}>
-      {headings.map((h) => <div key={h}>{h}</div>)}
-    </TableHeader>
-    <TableBody theme={theme} columns={headings.length}>
-      {Object.keys(data).map((id) => data[id].map((field, col) => <div key={`${id}.${col}`}>{field}</div>))}
-    </TableBody>
-  </TableContainer>
-));
+const StyledTableHeaderCell = styled.th(`
+  padding: 16px;
+  text-align: left;
+  font-weight: 400;
+  border-top: solid 1px #bdbdbd;
+  border-bottom: solid 1px #bdbdbd;
+
+  :first-of-type {
+    border-left: solid 1px #bdbdbd;
+    border-top-left-radius: 2px;
+    border-bottom-left-radius: 2px;
+  }
+
+  :last-of-type {
+    border-right: solid 1px #bdbdbd;
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
+`);
+
+const StyledTableBody = styled.tbody();
+
+const StyledTableRow = styled.tr();
+
+const StyledTableCell = styled.td<Themeable>(`
+  padding: 16px;
+  border-bottom: solid 1px #c7c7c7;
+`);
+
+const assignThemeToChildren = (theme?: QuartzTheme) => (
+  (child: React.ReactElement) => React.cloneElement(child, { theme })
+);
+
+export const Table: React.FC<TableProps> & TableSubComponents = ({ children, theme, ...rest }) => (
+  <StyledTable cellSpacing={0} {...rest}>
+    {React.Children.toArray(children).map(assignThemeToChildren(theme))}
+  </StyledTable>
+);
+
+Table.Header = ({ children, theme, ...rest }) => (
+  <StyledTableHeader theme={theme} {...rest}>
+    <tr>
+      {React.Children.toArray(children).map(assignThemeToChildren(theme))}
+    </tr>
+  </StyledTableHeader>
+);
+
+Table.HeaderCell = ({ children, theme, ...rest }) => (
+  <StyledTableHeaderCell theme={theme} {...rest}>
+    {children}
+  </StyledTableHeaderCell>
+);
+
+Table.Body = ({ children, theme, ...rest }) => (
+  <StyledTableBody {...rest}>
+    {React.Children.toArray(children).map(assignThemeToChildren(theme))}
+  </StyledTableBody>
+);
+
+Table.Row = ({ children, theme, ...rest }) => (
+  <StyledTableRow {...rest}>
+    {React.Children.toArray(children).map(assignThemeToChildren(theme))}
+  </StyledTableRow>
+);
+
+Table.Cell = ({ children, ...rest }) => (
+  <StyledTableCell {...rest}>
+    {children}
+  </StyledTableCell>
+);
 
 Table.defaultProps = {
   theme: defaultTheme
 };
-
-export default Table;
