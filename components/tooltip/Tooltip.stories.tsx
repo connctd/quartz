94094293f672785e508c45085ec6cfb94714b855
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2020-12-16 12:35:50 +0100

    feat: add Tooltip component

---
+++ components/tooltip/Tooltip.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import { Tooltip } from './index';
+
+const stories = storiesOf('Tooltip', module);
+
+stories.addDecorator(withKnobs);
+stories.addDecorator(withInfo);
+stories.addParameters({ info: { inline: true } });
+
+stories.add('Tooltip', () => (
+  <Tooltip content="I'm a Tooltip ...">
+    Hover me!
+  </Tooltip>
+));
