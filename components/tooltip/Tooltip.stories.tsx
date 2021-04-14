84a9dcb89ac514b7b6ec707cf1afe3bddfd9cbe9
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2021-04-14 15:24:02 +0200

    refactor: improve Tooltip storybook

--- components/tooltip/Tooltip.stories.tsx
+++ components/tooltip/Tooltip.stories.tsx
@@ -1,17 +1,84 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import { Tooltip } from './index';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 300px;
+`;
 
 const stories = storiesOf('Tooltip', module);
 
 stories.addDecorator(withKnobs);
 stories.addDecorator(withInfo);
 
-stories.add('Tooltip', () => (
-  <Tooltip content="I'm a Tooltip ...">
-    Hover me!
-  </Tooltip>
-));
+stories.add('Top', () => {
+  const content = text('Content', 'I am a Tooltip ...');
+  const position = 'top';
+
+  return (
+    <Container>
+      <Tooltip
+        position={position}
+        content={content}
+      >
+        Hover me!
+      </Tooltip>
+    </Container>
+  );
+});
+
+stories.add('Right', () => {
+  const content = text('Content', 'I am a Tooltip ...');
+  const position = 'right';
+
+  return (
+    <Container>
+      <Tooltip
+        position={position}
+        content={content}
+      >
+        Hover me!
+      </Tooltip>
+    </Container>
+  );
+});
+
+stories.add('Bottom', () => {
+  const content = text('Content', 'I am a Tooltip ...');
+  const position = 'bottom';
+
+  return (
+    <Container>
+      <Tooltip
+        position={position}
+        content={content}
+      >
+        Hover me!
+      </Tooltip>
+    </Container>
+  );
+});
+
+stories.add('Left', () => {
+  const content = text('Content', 'I am a Tooltip ...');
+  const position = 'left';
+
+  return (
+    <Container>
+      <Tooltip
+        position={position}
+        content={content}
+      >
+        Hover me!
+      </Tooltip>
+    </Container>
+  );
+});
