ccc8ab4c51741645cfe5fb05362c6fac9fce1af8
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2021-04-18 13:43:59 +0200

    chore: set version to prerelease v2.0.0-0

--- components/action/index.tsx
+++ components/action/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { css, ThemeContext } from '@emotion/core';
+import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { defaultTheme, Themeable } from '../theme';
 
