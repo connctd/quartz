9a002883874a46a31c1b7123c11e2bcb279e2c4f
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2021-09-20 16:00:41 +0200

    feat: export SearchField component from index file

--- src/index.tsx
+++ src/index.tsx
@@ -46,6 +46,9 @@ export { default as Paper } from './components/paper';
 export * from './components/pill';
 export { default as Pill } from './components/pill';
 
+export * from './components/searchField';
+export { default as SearchField } from './components/searchField';
+
 export * from './components/select';
 export { default as Select } from './components/select';
 
