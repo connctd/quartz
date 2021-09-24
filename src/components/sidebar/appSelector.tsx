1a317f006ae5d4639f219a2a783c13ddc0f35db8
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2021-09-24 11:20:24 +0200

    chore: bump version to prerelease v2.0.0-6

--- src/components/sidebar/appSelector.tsx
+++ src/components/sidebar/appSelector.tsx
@@ -207,7 +207,7 @@ const AppSelectorEmptyState = styled.div<Themeable>`
   right: 0;
   padding: 64px 32px;
   height: 296px;
-  background-color: ${({ theme }) => theme.purple};
+  background-color: ${({ theme }) => theme.purpleDark};
   color: ${({ theme }) => theme.white};
   text-align: center;
 `;
@@ -220,7 +220,7 @@ const AppSelectorLoadingState = styled.div<Themeable>`
   left: 0;
   right: 0;
   height: 296px;
-  background-color: ${({ theme }) => theme.purple};
+  background-color: ${({ theme }) => theme.purpleDark};
   text-align: center;
 `;
 
